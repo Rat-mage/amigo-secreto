a1176d401ea3117f8a922ffc50154a19830fe63e
Thiago Denir Ramos <thiago.denir.ramos@hotmail.com>
2021-12-08 17:01:09 -0300

    ajuste de retorno

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -8,6 +8,9 @@ type Data = {
 	id?: number;
 	name?: string;
 	phone?: string;
+	friend?: string;
+	updatedAt?: object;
+	createdAt?: object;
 };
 
 export default async function handler(
@@ -19,17 +22,18 @@ export default async function handler(
 		try {
 			await DB.sync();
 			//Caso não exista a tabela de amigos, ele cria a tabela.
-			const createFriend = await Amigo.create({
+			const data = await Amigo.create({
 				name,
 				phone,
 				friend: encrypt("Ainda não saiu o sorteio!"),
 			});
+			const { dataValues<Data> } = data;
 			//
-			console.log("opa", createFriend);
+			console.log("opa", dataValues);
+			res.status(200).json({ message: "Salvo com sucesso", ...dataValues });
 		} catch (error) {
 			console.log(error);
 		}
-		res.status(200).json({ message: "John Doe" });
 	} else {
 		res.status(400).json({ message: "ERRO" });
 	}
