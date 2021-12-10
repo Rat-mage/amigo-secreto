f5e69ca3667d5c6a08fb1976f262fea7e69ba786
Thiago Denir Ramos <thiago.denir.ramos@hotmail.com>
2021-12-10 14:41:20 -0300

    AJUSTE CORS

--- src/pages/api/result.js
+++ src/pages/api/result.js
@@ -6,7 +6,7 @@ import Cors from "cors";
 
 // Initializing the cors middleware
 const cors = Cors({
-	methods: ["GET", "HEAD"],
+	methods: ["GET"],
 });
 
 // Helper method to wait for a middleware to execute before continuing
