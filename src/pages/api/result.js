d3ff48e8cf2c15682542f9f7fce21a5196e72b3a
Thiago Denir Ramos <thiago.denir.ramos@hotmail.com>
2021-12-10 14:38:53 -0300

    cors

--- src/pages/api/result.js
+++ src/pages/api/result.js
@@ -1,24 +1,46 @@
 const { decrypt } = require("../../utils/cryptografer");
 const Amigo = require("../../db/model/amigo");
-const DB = require('../../db/connection')
+const DB = require("../../db/connection");
+
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+	methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
 
 export default async function handler(req, res) {
+	await runMiddleware(req, res, cors);
 	if (req.method === "GET") {
 		const { phone } = req.query;
-		const data = await Amigo.findOne({ where: { phone } })
-		var amigosecreto = ''
+		const data = await Amigo.findOne({ where: { phone } });
+		var amigosecreto = "";
 
 		if (data.visualized) {
-			amigosecreto = 'javisto'
+			amigosecreto = "javisto";
 		} else {
-			data.friend ? amigosecreto = decrypt(data.friend) : amigosecreto = 'semsorteio'
+			data.friend
+				? (amigosecreto = decrypt(data.friend))
+				: (amigosecreto = "semsorteio");
 		}
 
 		return res.json({
-			amigosecreto
-		})
-
-
+			amigosecreto,
+		});
 	} else {
 		res.status(400).json({ message: "ERRO" });
 	}
