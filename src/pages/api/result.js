100482671ba13f6772f20da7cb755439ec98abf7
Thiago Denir Ramos <thiago.denir.ramos@hotmail.com>
2021-12-10 15:21:57 -0300

    cors

--- src/pages/api/result.js
+++ src/pages/api/result.js
@@ -6,7 +6,7 @@ import Cors from "cors";
 
 // Initializing the cors middleware
 const cors = Cors({
-	methods: ["GET"],
+	methods: ["post"],
 });
 
 // Helper method to wait for a middleware to execute before continuing
